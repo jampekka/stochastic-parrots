b9b5fb980943d78241f19f36837e1df477d16ad7
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2024-05-16 13:29:51 +0300

    Remove main from simple_lm

--- simple_lm.py
+++ simple_lm.py
@@ -43,27 +43,3 @@ def generate_tokens(freq_table, initial_context):
 
         yield next_word
 
-
-
-def main():
-    input_file = "sample_data/blowin_in_the_wind_verses.txt"
-    context_length = 2
-    max_tokens = 100
-    #tokenizer = WhitespaceTokenizer()
-    tokenizer = SpaceTokenizer()
-
-    text = open(input_file).read()
-    tokens = tokenizer.tokenize(text)
-
-    initial_context = tokens[:context_length]
-
-    freq_table = get_next_token_table(tokens, context_length)
-
-    generator = generate_tokens(freq_table, initial_context)
-    output_tokens = itertools.islice(generator, max_tokens)
-    output_text = tokenizer.untokenize(output_tokens)
-
-    print(output_text)
-
-if __name__ == "__main__":
-    main()
