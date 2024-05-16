06dd140b5fead82526cf53c35cba79b32cfa553a
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2024-05-16 16:03:23 +0300

    Character tokenization and incremental frequency table generation

--- simple_lm.py
+++ simple_lm.py
@@ -16,15 +16,21 @@ class SpaceTokenizer:
     def untokenize(self, tokens):
         return ' '.join(tokens)
 
-
+class CharacterTokenizer:
+    def tokenize(self, text):
+        return list(text)
+    
+    def untokenize(self, tokens):
+        return ''.join(tokens)
 
 def get_ngrams(tokens, n):
     for i in range(len(tokens) - n + 1):
         context = tokens[i:i+n]
         yield context
 
-def get_next_token_table(tokens, context_length):
-    freq_table = defaultdict(list)
+def get_next_token_table(tokens, context_length, freq_table=None):
+    if freq_table is None:
+        freq_table = defaultdict(list)
     for *context, next_word in get_ngrams(tokens, context_length+1):
         freq_table[tuple(context)].append(next_word)
     return freq_table
